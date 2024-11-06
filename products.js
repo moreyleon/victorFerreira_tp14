a61cc0170266707e9d56dc4cef3fae82ca2d5346
--add <victor_ferreira@live.com.ar>
2024-11-06 00:12:27 -0300

    arrays

---
+++ products.js
@@ -0,0 +1,63 @@
+
+let electro = ["microonda", "cafetera", "televisor", "secarropa", "lavarropa", "batidora"]
+//a
+/*
+console.log(electro[2]);
+console.log(electro[5]);
+*/
+//b
+/*
+    let ultimo = electro.shift();
+    electro.push("microondas");
+    console.log(electro);
+  */
+
+
+//c
+/*
+electro.push("tablet","pava");
+
+console.log(electro);
+*/
+//d
+
+//console.log(electro);
+
+//e
+
+/*
+if (electro[5]) {
+    console.log("Producto encontrado");
+    
+} else {
+     console.log("El producto que buscado no existe");
+     
+}*/
+
+
+//f
+
+// console.log(electro.join(" "));
+
+//g
+
+//console.log(electro.length);
+
+//h
+
+/*electro.splice(1, 1, "cocina");
+console.log(electro.join(" "));*/
+
+//i
+
+ electro.splice(1, 1, "cocina");
+
+
+
+console.log(electro.join(","));
+
+
+
+
+
+
